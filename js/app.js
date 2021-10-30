528a161d01847c864ddff631083b2028725c6c44
Mantas70 <mantas.kvedaras98@gmail.com>
2021-10-30 10:45:26 +0300

    Third commit

--- js/app.js
+++ js/app.js
@@ -5,74 +5,74 @@ let books  = [
     {
         'Fantastine':[
     {
-        'ISBN': 1,
-        'Leidimo metai': 2021,
+        'ISBN': '1',
+        'Leidimo metai': '2021',
         'Pavadinimas': 'Metai',
-        'Puslapiu skaicius': 165
+        'Puslapiu skaicius': '165'
     },
     {
-        'ISBN': 2,
-        'Leidimo metai': 2014,
+        'ISBN': '2',
+        'Leidimo metai': '2014',
         'Pavadinimas': 'Metai',
-        'Puslapiu skaicius': 165
+        'Puslapiu skaicius': '165'
     },
     {
-        'ISBN': 3,
-        'Leidimo metai': 2013,
+        'ISBN': '3',
+        'Leidimo metai': '2013',
         'Pavadinimas': 'Metai',
-        'Puslapiu skaicius': 165
+        'Puslapiu skaicius': '165'
     },
     {
-        'ISBN': 4,
-        'Leidimo metai': 2012,
+        'ISBN': '4',
+        'Leidimo metai': '2012',
         'Pavadinimas': 'Metai',
-        'Puslapiu skaicius': 165
+        'Puslapiu skaicius': '165'
     }
     ],
         'Kriminaline':[
         {
-            'ISBN': 5,
-            'Leidimo metai': 2021,
+            'ISBN': '5',
+            'Leidimo metai': '2021',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
-            'ISBN': 6,
-            'Leidimo metai': 2011,
+            'ISBN': '6',
+            'Leidimo metai': '2011',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
-            'ISBN': 7,
-            'Leidimo metai': 2010,
+            'ISBN': '7',
+            'Leidimo metai': '2010',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
-            'ISBN': 8,
-            'Leidimo metai': 2008,
+            'ISBN': '8',
+            'Leidimo metai': '2008',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         }
     ],
         'Grozine':[
         {
-            'ISBN': 9,
-            'Leidimo metai': 2009,
+            'ISBN': '9',
+            'Leidimo metai': '2009',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
-            'ISBN': 10,
-            'Leidimo metai': 2001,
+            'ISBN': '10',
+            'Leidimo metai': '2001',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
             'ISBN': '11',
-            'Leidimo metai': 2001,
+            'Leidimo metai': '2001',
             'Pavadinimas': 'Metai',
-            'Puslapiu skaicius': 165
+            'Puslapiu skaicius': '165'
         },
         {
             'ISBN': '12',
@@ -83,13 +83,17 @@ let books  = [
                 ]
     }
 ]
+let item = []
 for (let object of books) for (let multiObject in object) {
-    console.log(multiObject.toUpperCase() + ': ')
     console.log(multiObject + ' Literatura' + ' (' + object[multiObject].length + ')')
     for (let number in object[multiObject]) {
         console.log(': ')
         for (let property in object[multiObject][number]) {
-            console.log(property+": ")
+                if(object[multiObject][number][property] === '2021'){
+                    console.log(property+": "+object[multiObject][number][property]+" (Nauja knyga)")
+            }else{
+                    console.log(property+": "+object[multiObject][number][property])
+                }
         }
     }
 }
