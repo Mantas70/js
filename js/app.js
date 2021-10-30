dd3016ec4c835e43a74afef20dcdeebf5f4ac93b
Mantas70 <mantas.kvedaras98@gmail.com>
2021-10-30 10:11:23 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,96 @@
+"use strict"
+
+
+let books  = [
+    {
+        'Fantastine':[
+    {
+        'ISBN': 1,
+        'Leidimo metai': 2021,
+        'Pavadinimas': 'Metai',
+        'Puslapiu skaicius': 165
+    },
+    {
+        'ISBN': 2,
+        'Leidimo metai': 2014,
+        'Pavadinimas': 'Metai',
+        'Puslapiu skaicius': 165
+    },
+    {
+        'ISBN': 3,
+        'Leidimo metai': 2013,
+        'Pavadinimas': 'Metai',
+        'Puslapiu skaicius': 165
+    },
+    {
+        'ISBN': 4,
+        'Leidimo metai': 2012,
+        'Pavadinimas': 'Metai',
+        'Puslapiu skaicius': 165
+    }
+    ],
+        'Kriminaline':[
+        {
+            'ISBN': 5,
+            'Leidimo metai': 2021,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': 6,
+            'Leidimo metai': 2011,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': 7,
+            'Leidimo metai': 2010,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': 8,
+            'Leidimo metai': 2008,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        }
+    ],
+        'Grozine':[
+        {
+            'ISBN': 9,
+            'Leidimo metai': 2009,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': 10,
+            'Leidimo metai': 2001,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': '11',
+            'Leidimo metai': 2001,
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': 165
+        },
+        {
+            'ISBN': '12',
+            'Leidimo metai': '2019',
+            'Pavadinimas': 'Metai',
+            'Puslapiu skaicius': '165'
+        }
+                ]
+    }
+]
+let property = []
+for (let object of books) for (let multiObject in object) {
+    console.log(multiObject.toUpperCase() + ': ')
+    console.log(multiObject + ' Literatura' + ' (' + object[multiObject].length + ')')
+    for (let number in object[multiObject]) {
+        console.log(': ')
+        for (property in object[multiObject][number]) {
+            console.log(property+": ")
+        }
+    }
+}
